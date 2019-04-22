fd1abe57a2da51f1c423b2f568cdd365295183af
Cedric Dlh <cedricdlh@gmail.com>
2019-04-22 21:03:14 +0200

    Created working test for Applicant controller

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,5 +1,16 @@
 class ApplicantsController < ApplicationController
 
   def create
+    @applicant = Applicant.new(applicant_params)
+   #@position.name = params
+   @applicant.save!
   end
+
+  private
+
+  def applicant_params
+    params.require(:applicant).permit(:first_name, :last_name, :email, :position_id)
+  end
+
+
 end
--- test/controllers/applicants_controller_test.rb
+++ test/controllers/applicants_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class ApplicantsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get applicants_create_url
+  test "should create valid applicant with First Name, Last Name, email and Position ID " do
+    test_position = Position.create(name: "Engineer", skill: "Javascript")
+    test_position.save
+
+    post applicants_create_url, params: { applicant: {first_name: "Cedric", last_name: "Dlh", email:"cedricdlh@gmail.com", position_id: test_position.id }}
+
     assert_response :success
+    assert_equal "Cedric", Applicant.last.first_name
+    assert_equal "Dlh", Applicant.last.last_name
+    assert_equal "cedricdlh@gmail.com", Applicant.last.email
+    assert_equal test_position.id, Applicant.last.position_id
+
   end
 
 end
