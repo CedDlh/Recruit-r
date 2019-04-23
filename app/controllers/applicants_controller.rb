cbbc0e2a0bd8df1e2e9f57645a7df197abc84ead
Cedric Dlh <cedricdlh@gmail.com>
2019-04-23 08:36:33 +0200

    Cleaned up test controller

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -2,15 +2,12 @@ class ApplicantsController < ApplicationController
 
   def create
     @applicant = Applicant.new(applicant_params)
-   #@position.name = params
-   @applicant.save!
+    @applicant.save!
   end
 
-  private
+private
 
   def applicant_params
     params.require(:applicant).permit(:first_name, :last_name, :email, :position_id)
   end
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_174457) do
+ActiveRecord::Schema.define(version: 2019_04_22_191135) do
 
   create_table "applicants", force: :cascade do |t|
     t.string "first_name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2019_04_22_174457) do
   create_table "positions", force: :cascade do |t|
     t.string "name"
     t.string "skill"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recruiters", force: :cascade do |t|
+    t.string "name"
+    t.string "skills"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
