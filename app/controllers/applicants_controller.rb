852ffd65b2f75546b68715e1e22a1d26d5ccdb64
Cedric Dlh <cedricdlh@gmail.com>
2019-04-23 17:48:12 +0200

    Created a show + update passing test dor applicants

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,13 +1,23 @@
 class ApplicantsController < ApplicationController
 
+  def show
+    @applicant = Applicant.find(params[:id])
+  end
+
   def create
     @applicant = Applicant.new(applicant_params)
     @applicant.save!
   end
 
+  def update
+    @applicant = Applicant.find(params[:id])
+    @applicant.update(applicant_params)
+    @applicant.save
+  end
+
 private
 
   def applicant_params
-    params.require(:applicant).permit(:first_name, :last_name, :email, :position_id)
+    params.require(:applicant).permit(:first_name, :last_name, :email, :position_id, :reruiter_id)
   end
 end
